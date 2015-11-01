5f4a35a5ecc0294969053708e8f0f6d841c2e3e5
Landon Powell <landonpowell@linuxmail.org>
2015-11-01 11:33:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> MessageBoard </title>
+        <title> FileBoard </title>
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
